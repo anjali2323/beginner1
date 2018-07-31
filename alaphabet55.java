d847d44114d26aed088ad84e792c6f76509fc512
anjali2323 <41777558+anjali2323@users.noreply.github.com>
2018-07-31 19:30:47 +0530

    Create alaphabet55.java

---
+++ alaphabet55.java
@@ -0,0 +1,31 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		char letter=sc.next().charAt(0);
+		if(letter>=65&&letter<=122)
+	{
+		if(letter=='a'||letter=='e'||letter=='i'||letter=='o'||letter=='u'||letter=='A'||letter=='E'||letter=='I'||letter=='o'||letter=='U')
+		{
+			System.out.println("Vowel");
+		}
+		else
+		{
+			System.out.println("Consonant");
+		}
+	}
+	else 
+		{
+			System.out.println("invalid");
+		}	
+	}
+
+}
