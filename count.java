9ed9666aa80fe0e3ad090da25fe170ef2b022bb7
anjali2323 <41777558+anjali2323@users.noreply.github.com>
2018-08-12 19:25:29 +0530

    Update count.java

--- count.java
+++ count.java
@@ -9,13 +9,13 @@ class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		int num,count=0;
+		int n,count=0;
 		{
-			Scanner sc=new Scanner(System.in);
-			num=sc.nextInt();
-			while(num>0)
+			Scanner s=new Scanner(System.in);
+			n=s.nextInt();
+			while(n>0)
 			{
-				num=num/10;
+				n=n/10;
 				count=count+1;
 			}
 			System.out.println(count);
