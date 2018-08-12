295e524f2d020da99031c28f9c326dea20bf73b9
anjali2323 <41777558+anjali2323@users.noreply.github.com>
2018-08-12 20:06:15 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -10,8 +10,8 @@ class Ideone
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		int n;
-		Scanner s=new Scanner(System.in);
-		n=s.nextInt();
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
 		for(int i=1;i<=n;i++)
 		{
 			System.out.println("Hello");
