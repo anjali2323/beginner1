b47207cbff24e5f9e81c1be6b0c99efc3872b140
anjali2323 <41777558+anjali2323@users.noreply.github.com>
2018-08-22 15:42:01 +0530

    Create order.java

---
+++ order.java
@@ -0,0 +1,30 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		
+		// your code goes here
+		int n;
+		Scanner s=new Scanner(System.in);
+		
+		n=s.nextInt();
+		LinkedList<Integer> stack = new LinkedList<Integer>();
+		while(n>0)
+	{
+		stack.push(n%10);
+		n=n/10;
+	}
+	while(!(stack.isEmpty()))
+	{
+		System.out.print(" "+stack.pop()+" ");
+	}
+	}
+}
