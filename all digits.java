c4fd0a4096c3de4513ae259655c7205ae075688d
anjali2323 <41777558+anjali2323@users.noreply.github.com>
2018-08-22 14:58:45 +0530

    Create all digits.java

---
+++ all digits.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a,b,c;
+		a=sc.nextInt();
+		b=sc.nextInt();
+		c=sc.nextInt();
+		System.out.print(" "+a);
+		System.out.print(" "+b);
+		System.out.print(" "+c);
+	}
+}
