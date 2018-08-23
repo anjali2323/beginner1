2ed92849e429fb83a6f15d1d29646020229eb79a
anjali2323 <41777558+anjali2323@users.noreply.github.com>
2018-08-23 19:41:25 +0530

    Create vowelyn.java

---
+++ vowelyn.java
@@ -0,0 +1,31 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		String str=s.nextLine();int fl=0;
+		for(int i=0;i<str.length();i++)
+		{
+			char ch=str.charAt(i);
+			if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
+			{
+				fl=1;
+				break;
+			}	
+		}
+		if(fl==1)
+		System.out.println("yes");
+		else
+		System.out.println("no");
+		
+	}
+}
+	
