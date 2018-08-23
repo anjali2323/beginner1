1885fe5d5f593a5fa48ee1479b874a7c3dba61cc
anjali2323 <41777558+anjali2323@users.noreply.github.com>
2018-08-23 10:42:39 +0530

    Create binary.java

---
+++ binary.java
@@ -0,0 +1,41 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		String str=s.next();
+		int f1=0,f0=0,te=0;
+		for(int i=0;i<str.length();i++)
+		{
+			char ch=str.charAt(i);
+			if(ch=='1')
+			{
+				f1=1;
+			}
+			else if(ch=='0')
+			{
+				f0=1;
+			}
+			else if(ch!='1' || ch!='0')
+			{
+				te=1;
+			}
+		}
+		if(f1==1 && f0==1 && te==0)
+		System.out.print("yes");
+		else
+		System.out.print("no");
+		
+		
+	}
+}
+
+	
