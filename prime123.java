534dab529a16971177e999c98e6e2f8ee6a1a9d1
anjali2323 <41777558+anjali2323@users.noreply.github.com>
2018-08-23 12:24:33 +0530

    Create prime123.java

---
+++ prime123.java
@@ -0,0 +1,28 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int cnt=0;
+		for(int i=1;i<=n/2;i++)
+		{
+			if(n%i==0)
+			cnt++;
+		}
+		if(cnt==1)
+		System.out.println("yes");
+		else
+		System.out.println("no");
+		
+	}
+}
+	
